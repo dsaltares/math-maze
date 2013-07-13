be77dd35cc6d59ccac700bc7a1fb2680ac367b65
David Saltares <david.saltares@gmail.com>
2013-07-13 17:22:35 +0100

    !T - Adds vibration when opening a door

--- mathmaze/src/com/siondream/math/systems/PlayerControllerSystem.java
+++ mathmaze/src/com/siondream/math/systems/PlayerControllerSystem.java
@@ -278,6 +278,7 @@ public class PlayerControllerSystem extends EntitySystem implements GestureListe
 			}
 			
 			engine.removeEntity(keyEntity);
+			Gdx.input.vibrate(GameEnv.playerVibrateTimeMs);
 		}
 		
 		if (isExitAt(destination)) {
